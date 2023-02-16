4c5f462a9ddc5aa1d192d7d3b2e5678a444e9680
Basliel25 <basgug25@gmail.com>
2023-02-16 23:32:15 +0300

    Finally

--- algo_toolbox/max_pair_prod.py
+++ algo_toolbox/max_pair_prod.py
@@ -1,14 +1,17 @@
 # created on: 11:56 11/02/23
 # Developed by: Basliel B. Gugsa
 def max_product(arr):
-    p1 = max(arr)
-    arr.remove(p1)
-    p2 = max(arr)
-    arr.remove(p2)
-    return p1*p2
-
+    if len(arr)>1:
+        p1 = max(arr)
+        arr.remove(p1)
+        p2 = max(arr)
+        arr.remove(p2)
+        return p1*p2
+    else:
+        return arr[0]
 
 if __name__ == '__main__':
+    size = int(input())
     numbers = list(map(int, input().split()))
     result = max_product(numbers)
     print(result)
